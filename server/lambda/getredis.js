6a7820ac7f511a9e9040ee69cb70a1fe58f671f8
王瑞 <rui6_wang.sgmwh@saic-gm.com>
2019-02-13 10:33:46 +0800

    修改相关信息
    由于需要改动，因此改动了。
    改动了1
    改动了2
    改动了3
    
    需要注意21232

--- server/lambda/getredis.js
+++ server/lambda/getredis.js
@@ -14,5 +14,6 @@ module.exports = async function(params,ctx){
      let res=await redis.get(key);
      console.log(res);
      //这里是fea1分支新增的内容
+     //这是直接在master更改的内容，为了研究git commit的时候message填写
      return ctx.succeed(res);
 }
