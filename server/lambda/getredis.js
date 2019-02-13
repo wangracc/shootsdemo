a289589b73c07e0f6bc20d851ca19c7ff614a2d4
王瑞 <rui6_wang.sgmwh@saic-gm.com>
2019-02-13 11:33:32 +0800

    修改

--- server/lambda/getredis.js
+++ server/lambda/getredis.js
@@ -2,7 +2,6 @@ const shootsRedis =  require('../lib/redis')
 module.exports = async function(params,ctx){
     console.log(params);
     let key;
-    console.log("dev分支新增的内容")
    try{
        key = params.key;
    } catch(err){
@@ -10,10 +9,7 @@ module.exports = async function(params,ctx){
     console.log(err);
    }
     let redis = shootsRedis();
-    console.log("test分支新增的内容");
      let res=await redis.get(key);
      console.log(res);
-     //这里是fea1分支新增的内容
-     //这是直接在master更改的内容，为了研究git commit的时候message填写
      return ctx.succeed(res);
 }
